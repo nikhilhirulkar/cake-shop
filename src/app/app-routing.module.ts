a67a26c17a51ba1e164f5d02dce310177eb1700e
hirulkar <nikhilhirulkar1994@gmail.com>
2020-09-13 22:30:50 +0530

    CHANGED CSS TO SASS FORMAT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent }  from './login/login.component';
 import { DashboardComponent }  from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 
@@ -10,6 +12,8 @@ const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'add-product', component: AddProductComponent },
+  { path: 'product', component: ProductListComponent },
 ];
 
 @NgModule({
