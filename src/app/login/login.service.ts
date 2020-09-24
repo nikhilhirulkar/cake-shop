427256d60bc13d86b0186022582273071885624b
nikhil <nikhilhirulkar1994@gmail.com>
2020-09-24 20:26:29 +0530

    ping login api

---
+++ src/app/login/login.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LoginService {
+  
+  constructor(
+    private http: HttpClient,
+  ) {
+    
+   }
+   
+  url="https://reqres.in/api/";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  }
+
+
+
+  public loginUser(username, password): Observable<any> {
+    // let loginParams = new HttpParams().set('username', username).set('password', password);
+
+    return this.http.post<any>(
+      this.url + 'login',
+      {
+        email : username,
+        password: password
+      },
+      this.httpOptions
+    );
+  }
+
+  // loginUser(username, password) {
+	// 	let queryParams = {
+	// 	};
+
+	// 	return this.http.post(`${this.url}login/`, {username : username,
+  //     password: password},
+	// 		{ params: queryParams, observe: 'response' });
+	// }
+
+  // public logOutUser(): Observable<any> {
+
+  //   return this.http.post<any>(
+  //     this.url + '/auth/user-logout/',
+  //     this.httpOptions
+  //   );
+  // }
+}
